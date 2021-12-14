ed6115a0eaba7121cfe669687b59538e7e470e4d
Arafat6065 <yasinarafat9182@gmail.com>
2021-12-14 11:17:14 +0600

    second-commit

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h2>This is inve inventory</h2>
+            <h2>This is inve inventory paze</h2>
         </div>
     );
 };
